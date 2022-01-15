0c634efbf688d841ea9bde1926874c165480fa6d
Timur Evgazhukov <evgajukov@gmail.com>
2022-01-15 21:29:24 +0300

    доработана страница начальной загрузки и "о приложении"

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,15 +1,47 @@
 import 'package:dom24x7_flutter/widgets/footer_widget.dart';
 import 'package:dom24x7_flutter/widgets/header_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class AboutPage extends StatelessWidget {
+class AboutPage extends StatefulWidget {
   const AboutPage({Key? key}) : super(key: key);
+
+  @override
+  _AboutPage createState() => _AboutPage();
+}
+
+class _AboutPage extends State<AboutPage> {
+  String _version = '0.0.0';
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _version = info.version;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Header(context, 'О проекте'),
+      appBar: Header(context, 'О приложении'),
       bottomNavigationBar: Footer(context, FooterNav.news),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Image(image: AssetImage('assets/images/logo.png')),
+            const Text('Dom24x7', style: TextStyle(fontSize: 50, color: Colors.blue)),
+            Text('Версия: $_version', style: const TextStyle(color: Colors.blue))
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/pages/app_loader_screen_page.dart
+++ lib/pages/app_loader_screen_page.dart
@@ -1,9 +1,30 @@
 import 'package:dom24x7_flutter/store/main.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
 
-class AppLoaderScreenPage extends StatelessWidget {
+class AppLoaderScreenPage extends StatefulWidget {
   const AppLoaderScreenPage({Key? key}) : super(key: key);
+
+  @override
+  _AppLoaderScreenPage createState() => _AppLoaderScreenPage();
+}
+
+class _AppLoaderScreenPage extends State<AppLoaderScreenPage> {
+  String _version = '0.0.0';
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _version = info.version;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +43,11 @@ class AppLoaderScreenPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Image(image: AssetImage('assets/images/logo.png')),
-            Text('Dom24x7', style: TextStyle(fontSize: 50, color: Colors.blue)),
-            Text('Версия 18 сборка 1', style: TextStyle(color: Colors.blue))
+          children: [
+            const Image(image: AssetImage('assets/images/logo.png')),
+            const Text('Dom24x7', style: TextStyle(fontSize: 50, color: Colors.blue)),
+            Text('Версия: $_version', style: const TextStyle(color: Colors.blue)),
+            const Text('идет загрузка данных...', style: TextStyle(color: Colors.black45)),
           ],
         ),
       ),
