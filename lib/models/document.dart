3e698185f74df9b0f61d01885300961f54c623cb
Timur Evgazhukov <evgajukov@gmail.com>
2022-01-28 10:32:46 +0300

    сервис списка документов

--- lib/models/document.dart
+++ lib/models/document.dart
@@ -1,12 +1,14 @@
 class Document {
   late int id;
   late String title;
+  String? annotation;
   late String url;
 
   Document(this.id, this.title, this.url);
   Document.fromMap(Map<String, dynamic> map) {
     id = map['id'];
     title = map['title'];
+    annotation = map['annotation'];
     url = map['url'];
   }
 }
--- lib/pages/services/documents_page.dart
+++ lib/pages/services/documents_page.dart
@@ -1,15 +1,33 @@
+import 'package:dom24x7_flutter/store/main.dart';
 import 'package:dom24x7_flutter/widgets/footer_widget.dart';
 import 'package:dom24x7_flutter/widgets/header_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DocumentsPage extends StatelessWidget {
   const DocumentsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<MainStore>(context);
+    final documents = store.documents.list!;
+
     return Scaffold(
       appBar: Header(context, 'Документы'),
       bottomNavigationBar: Footer(context, FooterNav.services),
+      body: ListView.builder(
+        itemCount: documents.length,
+        itemBuilder: (BuildContext context, int index) {
+          final document = documents[index];
+          return ListTile(
+            leading: const Icon(Icons.description),
+            title: Text(document.title),
+            subtitle: document.annotation != null ? Text(document.annotation!) : null,
+            onTap: () => { launch(document.url) }
+          );
+        }
+      )
     );
   }
 }
