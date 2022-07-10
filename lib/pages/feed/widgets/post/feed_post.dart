0f79eb5f62a58faa58b6b9d14f33f00b0af87235
Тимур Евгажуков <evgajukov@gmail.com>
2022-07-10 15:06:07 +0300

    передаем автора поста

--- lib/pages/feed/widgets/post/feed_post.dart
+++ lib/pages/feed/widgets/post/feed_post.dart
@@ -2,9 +2,7 @@ import 'package:dom24x7_flutter/models/post/post.dart';
 import 'package:dom24x7_flutter/pages/feed/widgets/post/feed_post_description.dart';
 import 'package:dom24x7_flutter/pages/feed/widgets/post/picture/picture_carousal.dart';
 import 'package:dom24x7_flutter/pages/feed/widgets/post/profile_slab.dart';
-import 'package:dom24x7_flutter/store/main.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class FeedPost extends StatefulWidget {
   final Post post;
@@ -17,8 +15,7 @@ class FeedPost extends StatefulWidget {
 class _FeedPostState extends State<FeedPost> {
   @override
   Widget build(BuildContext context) {
-    final store = Provider.of<MainStore>(context);
-    List<Widget> widgets = [ProfileSlab(user: store.user.value!)];
+    List<Widget> widgets = [ProfileSlab(user: widget.post.enrichedActivity!.actor!)];
     if (widget.post.enrichedActivity != null) {
       widgets.add(PictureCarousal(enrichedActivity: widget.post.enrichedActivity!));
       widgets.add(Description(enrichedActivity: widget.post.enrichedActivity!));
